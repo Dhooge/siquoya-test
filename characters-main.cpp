d63cfcf308b14f47a4007a8cbc729370d4f2ea67
Adelie (Laptop) <adelie@siquoya.fr>
2013-05-09 18:36:43 +0200

    Character function practice

---
+++ characters-main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main()
+{
+  char character;
+
+  std::cout << "Enter a character:  ";
+  std::cin >> character;;
+
+  if(isdigit(character))
+    std::cout << character << " is a digit." << std::endl;
+  else if(isalpha(character))
+    { if(isupper(character))
+        std::cout << character << " is an uppercase letter." << std::endl;
+      else std::cout << character << " is a lowercase letter." << std::endl; }
+
+  return 0;
+}
